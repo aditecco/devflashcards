a2d3c66e9102118bc2d12793352ae8a2a464a743
Alessandro Di Tecco <alessandro@ditecco.me>
2021-05-29 20:05:26 +0200

    Added theme boilerplate

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,4 +2,28 @@
 theme
 --------------------------------- */
 
-export default {};
+export default {
+  // based on: https://system-ui.com/theme
+  space: {},
+  fontSizes: {},
+  colors: {
+    accent: {
+      "01": "",
+    },
+    background: {
+      "01": "",
+    },
+  },
+  fonts: {},
+  fontWeights: {},
+  lineHeights: {},
+  letterSpacings: {},
+  sizes: {},
+  borders: {},
+  borderWidths: {},
+  borderStyles: {},
+  radii: {},
+  shadows: {},
+  zIndices: {},
+  transitions: {},
+};
