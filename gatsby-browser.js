18cdc8e2ae315a6563265616b3e667ae9c476df1
Alessandro Di Tecco <alessandro@ditecco.me>
2021-05-29 18:11:56 +0200

    Added ThemeProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,31 @@
-// prismjs theme
-require("prismjs/themes/prism-tomorrow.css");
+import React from "react";
+import { ThemeProvider, Global, css } from "@emotion/react";
+import theme from "./src/theme/theme";
+// fonts
+import "@fontsource/material-icons/index.css";
+// 3rd party CSS
+import "normalize.css/normalize.css";
+import "prismjs/themes/prism-tomorrow.css";
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <Global
+      styles={css`
+        // other globals
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+
+        body {
+          //font-family: ${({ theme }) => theme.bodyFont};
+          margin: 0;
+          padding: 0;
+        }
+      `}
+    />
+
+    {element}
+  </ThemeProvider>
+);
