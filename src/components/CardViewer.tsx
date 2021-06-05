75330ac94df4a1c7e79a1e703df36c3d95a0a9de
Alessandro Di Tecco <alessandro@ditecco.me>
2021-06-05 18:26:38 +0200

    Redesigned card stacking logic

--- src/components/CardViewer.tsx
+++ src/components/CardViewer.tsx
@@ -3,7 +3,7 @@ CardViewer
 --------------------------------- */
 
 import * as React from "react";
-import { PropsWithChildren, ReactElement, useRef } from "react";
+import { PropsWithChildren, ReactElement, useRef, useState } from "react";
 import CardWithFlip from "./CardWithFlip";
 import { css } from "@emotion/react";
 import { motion, useMotionValue, useTransform } from "framer-motion";
@@ -16,18 +16,36 @@ type OwnProps = {
 export default function CardViewer({
   cards,
 }: PropsWithChildren<OwnProps>): ReactElement | null {
-  const containerRef = useRef(null);
-  const x = useMotionValue(0);
-  const background = useTransform(
-    x,
-    [-100, 0, 100],
-    ["#ff008c", "#7700ff", "rgb(230, 255, 0)"]
+  const [cardStackingOrder, setCardStackingOrder] = useState(
+    createStackingOrderMap(cards)
   );
+  const containerRef = useRef(null);
+
+  function createStackingOrderMap(items) {
+    const zIndexes = items.map((_, i) => i).reverse();
+
+    return items.reduce((acc, _, i) => {
+      acc[i] = zIndexes[i];
+
+      return acc;
+    }, {});
+  }
+
+  console.log(cardStackingOrder);
+
+  // const x = useMotionValue(0);
+  // const background = useTransform(
+  //   x,
+  //   [-100, 0, 100],
+  //   ["#ff008c", "#7700ff", "rgb(230, 255, 0)"]
+  // );
+
+  // console.log("x", x);
 
   return (
-    <div
+    <motion.div
       className="card-viewer"
-      // ref={containerRef}
+      ref={containerRef}
       css={css`
         background: radial-gradient(at center, white, whitesmoke);
         height: 100vh;
@@ -41,36 +59,42 @@ export default function CardViewer({
           height: 400px;
         }
       `}
+      // style={{ background }}
     >
-      <div className="card-container" ref={containerRef}>
-        {cards?.map?.((card, i, cards) => {
-          const zIndexes = [];
-
-          for (let j = cards.length; j > 0; j--) {
-            zIndexes.push(j);
-          }
-
+      <motion.div className="card-container" ref={containerRef}>
+        {cards?.map?.((card, i) => {
           return (
             <motion.div
-              key={card?.node?.id}
+              key={i}
               drag="x"
-              dragConstraints={containerRef}
+              dragConstraints={{ left: 0, right: 0 }}
+              // dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
+              // dragConstraints={containerRef}
               data-card={"card__" + i}
+              dragElastic={0.8}
+              onDragEnd={(_, info) => {
+                const dragMax = info.point.x;
+
+                if (dragMax > 200 || dragMax > -200) {
+                  setCardStackingOrder((order) => {
+                    return Object.values(order).reduce((acc, curr, j) => {
+                      acc[j] = j === i ? 0 : curr + 1;
+
+                      return acc;
+                    }, {});
+                  });
+                }
+              }}
               style={{
-                ...x,
                 position: "absolute",
                 inset: 0,
-                zIndex: zIndexes[i],
-              }}
-              dragElastic={0.7}
-              onDragEnd={(event, info) => {
-                console.log(event, info);
+                zIndex: cardStackingOrder[i],
               }}
             >
+              <small>zIndex: {cardStackingOrder[i]}</small>
               <CardWithFlip
                 card={card?.node}
                 noShadow={i !== 0}
-
                 // style={{
                 //   ...(i !== cards.length - 1 ? { boxShadow: "none" } : {}),
                 // }}
@@ -78,7 +102,7 @@ export default function CardViewer({
             </motion.div>
           );
         })}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
