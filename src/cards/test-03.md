9c4121614bc2ec3d64b3545cf6d2782d2c44e104
Alessandro Di Tecco <alessandro@ditecco.me>
2021-06-10 01:06:11 +0200

    Quick experiment in trying to manipulate the HTML string directly; horrible code

--- src/cards/test-03.md
+++ src/cards/test-03.md
@@ -8,6 +8,36 @@ options: null
 answer: Dolor sit amet
 ---
 
+###### 3. What's the output?
+
 ```javascript
-const zero = 0;
+const shape = {
+  radius: 10,
+  diameter() {
+    return this.radius * 2;
+  },
+  perimeter: () => 2 * Math.PI * this.radius,
+};
+
+console.log(shape.diameter());
+console.log(shape.perimeter());
 ```
+
+- A: `20` and `62.83185307179586`
+- B: `20` and `NaN`
+- C: `20` and `63`
+- D: `NaN` and `63`
+
+<details><summary><b>Answer</b></summary>
+<p>
+
+#### Answer: B
+
+Note that the value of `diameter` is a regular function, whereas the value of `perimeter` is an arrow function.
+
+With arrow functions, the `this` keyword refers to its current surrounding scope, unlike regular functions! This means that when we call `perimeter`, it doesn't refer to the shape object, but to its surrounding scope (window for example).
+
+There is no value `radius` on that object, which returns `NaN`.
+
+</p>
+</details>
--- src/components/CardWithFlip.tsx
+++ src/components/CardWithFlip.tsx
@@ -28,6 +28,16 @@ export default function CardWithFlip({
     html,
   } = card ?? {};
 
+  const cleared = html
+    .replace(/<h6>[a-zA-Z]{.}<\/h6>/g, "")
+    .replace(/<h6>[a-zA-Z0-9\.]{.}<\/h6>/g, "")
+    .replace(/<details><summary><b>Answer<\/b><\/summary>/g, "")
+    .replace(/<p>/, "")
+    .replace(/<\/p>/, "")
+    .replace(/<\/details>/, "");
+  const q = cleared.split(/\<h4\>Answer\:\s[A-Z]{1}\<\/h4\>/g).shift();
+  const a = cleared.split(/\<h4\>Answer\:\s[A-Z]{1}\<\/h4\>/g).pop();
+
   return (
     <Flip style={style ?? {}}>
       <Flip.Front>
@@ -38,7 +48,7 @@ export default function CardWithFlip({
               css={css`
                 overflow-y: auto;
 
-                > *:not(.card-content-HTML) {
+                > *:not(.card-content-html) {
                   padding: 0 1rem;
                 }
 
@@ -57,8 +67,8 @@ export default function CardWithFlip({
                   margin: 0;
                 }
 
-                .card-content-HTML {
-                  font-size: small;
+                .card-content-html {
+                  overflow-y: auto;
                 }
 
                 .card-content-options {
@@ -72,8 +82,8 @@ export default function CardWithFlip({
               <h4 className="card-content-title">{question}</h4>
 
               <div
-                className={"card-content-HTML"}
-                dangerouslySetInnerHTML={{ __html: html }}
+                className={"card-content-html"}
+                dangerouslySetInnerHTML={{ __html: q }}
               />
 
               <div className="card-content-options">{options}</div>
@@ -97,14 +107,27 @@ export default function CardWithFlip({
       <Flip.Back>
         {({ setFlipped }) => (
           <Card>
-            <CardFlipControls back onFlip={() => setFlipped((f) => !f)} />
-
-            <span>{answer}</span>
+            {/*<span>{answer}</span>*/}
+            <div className="card-content-html" style={{ overflowY: "auto" }}>
+              <div
+                className={"card-content-html-inner"}
+                dangerouslySetInnerHTML={{ __html: a }}
+                style={{
+                  padding: "2rem 1.5rem",
+                  lineHeight: "1.6",
+                  fontSize: ".9rem",
+                }}
+              />
+            </div>
 
             <CardFooter>
               <CardControls>
-                <button className="CardControlsButton" type="button">
-                  Again
+                <button
+                  className="CardControlsButton"
+                  type="button"
+                  onClick={() => setFlipped((f) => !f)}
+                >
+                  Flip
                 </button>
 
                 <button className="CardControlsButton" type="button">
