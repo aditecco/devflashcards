16029ecd9407b29d443d2ae23af53d48eb665933
Alessandro Di Tecco <alessandro@ditecco.me>
2021-06-15 00:34:17 +0200

    Created basic sanitizer

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,13 @@ export function truncate(s: string): string | undefined {
 
   return s.length > 20 ? s.substr(0, 18) + "…" : s;
 }
+
+export function basicSanitizer(html: string): string | undefined {
+  if (!html) return;
+
+  const patterns = {
+    "empty tags": /<([a-z]+)><\/\1>/g,
+  };
+
+  return html.replace(patterns["empty tags"], "");
+}
