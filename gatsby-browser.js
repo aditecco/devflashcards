1038f10e22f4176284ad530e7f4a43b5385cd31f
Alessandro Di Tecco <alessandro@ditecco.me>
2021-06-29 00:21:21 +0200

    Added new font family

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,6 +10,7 @@ import theme from "./src/theme";
 import "@fontsource/material-icons/index.css";
 import "@fontsource/open-sans/index.css";
 import "@fontsource/lato/index.css";
+import "@fontsource/lato/400-italic.css";
 
 // 3rd party CSS
 import "normalize.css/normalize.css";
