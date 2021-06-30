58e32f4d208353ac4eb89104d7f8a72a9944aaab
Alessandro Di Tecco <alessandro@ditecco.me>
2021-07-01 00:32:32 +0200

    Incremented truncate cap

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,10 +2,10 @@
 utils
 --------------------------------- */
 
-export function truncate(s: string): string | undefined {
+export function truncate(s: string, cap = 18): string | undefined {
   if (!s) return;
 
-  return s.length > 20 ? s.substr(0, 18) + "…" : s;
+  return s.length > 20 ? s.substr(0, cap) + "…" : s;
 }
 
 export function basicSanitizer(html: string): string | undefined {
