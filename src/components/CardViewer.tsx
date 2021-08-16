1c6163653f64d4110bd69970d8367939be89fdea
Alessandro Di Tecco <alessandro@ditecco.me>
2021-08-16 03:13:36 +0200

    feature/cardviewer_refactor | Simplified card stacking, removed dragControls, other optims

--- src/components/CardViewer.tsx
+++ src/components/CardViewer.tsx
@@ -3,19 +3,14 @@ CardViewer
 --------------------------------- */
 
 import * as React from "react";
-import { PropsWithChildren, ReactElement, useRef, useState } from "react";
+import { PropsWithChildren, ReactElement, useRef } from "react";
 import CardWithFlip from "./CardWithFlip";
 import { css } from "@emotion/react";
-import {
-  motion,
-  useDragControls,
-  useMotionValue,
-  useTransform,
-} from "framer-motion";
+import { motion } from "framer-motion";
 import { $navbarHeight, CARD_HEIGHT, CARD_WIDTH } from "../constants/css-vars";
 import { Flashcard } from "../types";
 import { SuperMemoGrade } from "supermemo";
-import {DRAG_TRIGGER} from "../constants/constants";
+import { DRAG_TRIGGER } from "../constants/constants";
 
 type OwnProps = {
   cards: Flashcard[];
@@ -26,37 +21,7 @@ export default function CardViewer({
   cards,
   onCardReview,
 }: PropsWithChildren<OwnProps>): ReactElement | null {
-  const [cardStackingOrder, setCardStackingOrder] = useState(
-    createStackingOrderMap(cards)
-  );
-  const containerRef = useRef(null);
-  const dragControls = useDragControls();
-
-  // createStackingOrderMap
-  function createStackingOrderMap(items: Flashcard[]): Record<string, number> {
-    const zIndexes = items.map((_, i) => i).reverse();
-
-    return items.reduce((acc, _, i) => {
-      acc[i] = zIndexes[i];
-
-      return acc;
-    }, {});
-  }
-
-  console.log(cardStackingOrder); // TODO
-
-  // const x = useMotionValue(0);
-  // const background = useTransform(
-  //   x,
-  //   [-100, 0, 100],
-  //   ["#ff008c", "#7700ff", "rgb(230, 255, 0)"]
-  // );
-
-  // console.log("x", x);
-
-  function startDrag(event) {
-    dragControls.start(event, { snapToCursor: true });
-  }
+  const dragBoundaries = useRef(null);
 
   return (
     <div
@@ -73,48 +38,44 @@ export default function CardViewer({
           width: ${CARD_WIDTH};
           height: ${CARD_HEIGHT};
         }
+
+        .card-wrapper {
+          position: absolute;
+          inset: 0;
+        }
       `}
     >
-      <motion.div className="card-container" ref={containerRef}>
-        {cards?.map?.((card, i) => {
+      <motion.div className="card-container" ref={dragBoundaries}>
+        {cards?.map?.((card, i, cards) => {
+          const first = i === 0;
+
           return (
             <motion.div
               key={i}
-              drag="x"
-              dragConstraints={{ left: 0, right: 0 }}
-              // dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
-              // dragConstraints={containerRef}
+              className={"card-wrapper"}
               data-card={"card__" + (i + 1)}
+              drag={first ? "x" : false}
               dragElastic={0.8}
-              dragControls={dragControls}
+              dragConstraints={dragBoundaries}
               onDragEnd={(_, info) => {
                 const dragMax = info.point.x;
 
                 if (Math.abs(dragMax) > DRAG_TRIGGER) {
-                  setCardStackingOrder((order) => {
-                    return Object.values(order).reduce((acc, curr, j) => {
-                      acc[j] = j === i ? 0 : curr + 1;
-
-                      return acc;
-                    }, {});
-                  });
+                  // rate the card w/ the lowest grade
+                  onCardReview(card, 0);
                 }
               }}
               style={{
-                position: "absolute",
-                inset: 0,
-                zIndex: cardStackingOrder[i],
-                cursor: "grab",
+                cursor: first ? "grab" : "pointer",
+              }}
+              animate={{
+                zIndex: cards.length - i,
               }}
             >
               <CardWithFlip
                 card={card}
                 noShadow={i !== 0}
                 onCardReview={onCardReview} // TODO avoid drilling
-                onCardDrag={startDrag}
-                // style={{
-                //   ...(i !== cards.length - 1 ? { boxShadow: "none" } : {}),
-                // }}
               />
             </motion.div>
           );
