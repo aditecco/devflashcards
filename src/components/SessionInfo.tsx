3186d074d80113513f2844bcaa5445ff173f78f9
Alessandro Di Tecco <alessandro@ditecco.me>
2021-08-24 13:06:00 +0200

    Added styles & WIP content to SessionInfo.tsx

--- src/components/SessionInfo.tsx
+++ src/components/SessionInfo.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 import { PropsWithChildren, ReactElement } from "react";
 import { Flashcard } from "../types";
 import dayjs from "dayjs";
-import { css } from "@emotion/react";
+import { css, useTheme } from "@emotion/react";
 
 type OwnProps = {
   cards: Flashcard[];
@@ -15,5 +15,77 @@ type OwnProps = {
 export default function SessionInfo({
   cards,
 }: PropsWithChildren<OwnProps>): ReactElement | null {
-  return <div css={css``}>SessionInfo</div>;
+  const theme = useTheme();
+
+  return (
+    <div
+      css={css`
+        border-bottom: 1px solid ${theme?.colors?.stroke?.[2]};
+
+        ul {
+          display: flex;
+          padding: 0;
+          margin: 0;
+          list-style: none;
+        }
+
+        li {
+          &:first-child {
+            padding-left: 1rem;
+          }
+
+          &:last-child {
+            padding-right: 1rem;
+          }
+
+          padding: 1.25rem 1.5rem;
+
+          + li {
+            //padding-left: 1rem;
+            border-left: 1px solid ${theme?.colors?.stroke?.[2]};
+          }
+
+          h6 {
+            margin: 0 0 0.5rem;
+            font-size: 0.7rem;
+            font-weight: normal;
+            color: ${theme?.colors?.typography?.[2]};
+            text-transform: uppercase;
+          }
+        }
+      `}
+    >
+      <ul>
+        <li>
+          <h6>Your session</h6>
+          <span>{dayjs().toString()}</span>
+        </li>
+
+        <li>
+          <h6>Due today</h6>
+          <span>
+            {
+              cards.filter((c) => dayjs(c.dueDate).unix() === dayjs().unix())
+                .length
+            }
+          </span>
+        </li>
+
+        <li>
+          <h6>Correct</h6>
+          <span></span>
+        </li>
+
+        <li>
+          <h6>Incorrect</h6>
+          <span></span>
+        </li>
+
+        <li>
+          <h6>Passed</h6>
+          <span></span>
+        </li>
+      </ul>
+    </div>
+  );
 }
