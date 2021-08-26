45a4a3bb13323aa80223b493ac2a71c596cb3b46
Alessandro Di Tecco <alessandro@ditecco.me>
2021-08-27 00:04:26 +0200

    Added styles, hidden functionality to DebugInfo.tsx

--- src/components/DebugInfo.tsx
+++ src/components/DebugInfo.tsx
@@ -3,10 +3,11 @@ DebugInfo
 --------------------------------- */
 
 import * as React from "react";
-import { PropsWithChildren, ReactElement } from "react";
+import { PropsWithChildren, ReactElement, useState } from "react";
 import { Flashcard } from "../types";
 import dayjs from "dayjs";
 import { css } from "@emotion/react";
+import { Button } from "./Button";
 
 type OwnProps = {
   cards: Flashcard[];
@@ -15,28 +16,44 @@ type OwnProps = {
 export default function DebugInfo({
   cards,
 }: PropsWithChildren<OwnProps>): ReactElement | null {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div
       css={css`
         position: fixed;
-        bottom: 10px;
-        left: 10px;
+        ${visible ? "bottom: 20px; left: 20px" : "bottom: 0; left: 0"};
+        background: rgba(255, 255, 255, 0.9);
+        padding: 1rem 1.5rem;
+        box-shadow: ${visible ? "0 2px 20px 10px rgba(0, 0, 0, 0.25)" : "none"};
+        border-radius: ${visible ? "6px" : "0"};
         font-size: small;
+        transition: all 0.15s ease-in-out;
         z-index: 1;
 
         ul {
           padding-left: 1rem;
         }
+
+        button {
+          margin-top: ${visible ? "1.5rem" : "0"};
+        }
       `}
     >
-      <ul>
-        {cards?.map((c, i) => (
-          <li key={i}>
-            {c.node.frontmatter.order + 1} | {c.node.id.substr(-5)} |{" "}
-            {dayjs(c.dueDate).toString()}
-          </li>
-        ))}
-      </ul>
+      {visible && (
+        <ul>
+          {cards?.map((c, i) => (
+            <li key={i}>
+              {c.node.frontmatter.order + 1} | {c.node.id.substr(-5)} |{" "}
+              {dayjs(c.dueDate).toString()}
+            </li>
+          ))}
+        </ul>
+      )}
+
+      <Button onClick={() => setVisible((v) => !v)}>
+        Show/Hide debug info
+      </Button>
     </div>
   );
 }
