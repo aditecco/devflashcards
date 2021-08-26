5ff3b172d616421ddca98f69317b95a7d41cbe79
Alessandro Di Tecco <alessandro@ditecco.me>
2021-08-27 01:04:22 +0200

    Refactored CardViewer.tsx layout

--- src/components/CardViewer.tsx
+++ src/components/CardViewer.tsx
@@ -31,18 +31,21 @@ export default function CardViewer({
         background: radial-gradient(at center, white, whitesmoke);
         height: calc(100vh - ${$navbarHeight});
         position: relative;
-        //display: flex;
-        //justify-content: center;
-        //align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        .card-area {
+          flex-grow: 1;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
         .card-container {
-          //position: relative;
-          position: absolute;
-          left: 50%;
-          top: 50%;
+          position: relative;
           width: ${CARD_WIDTH};
           height: ${CARD_HEIGHT};
-          transform: translate(-50%, -50%);
         }
 
         .card-wrapper {
@@ -51,43 +54,46 @@ export default function CardViewer({
         }
       `}
     >
-      <motion.div className="card-container" ref={dragBoundaries}>
-        {cards?.map?.((card, i, cards) => {
-          const first = i === 0;
+      <div className="card-area">
+        <motion.div className="card-container" ref={dragBoundaries}>
+          {cards?.map?.((card, i, cards) => {
+            const first = i === 0;
 
-          return (
-            <motion.div
-              key={i}
-              className={"card-wrapper"}
-              data-card={"card__" + (i + 1)}
-              drag={first ? "x" : false}
-              dragElastic={0.8}
-              dragConstraints={dragBoundaries}
-              onDragEnd={(_, info) => {
-                const dragMax = info.point.x;
+            return (
+              <motion.div
+                key={i}
+                className={"card-wrapper"}
+                data-card={"card__" + (i + 1)}
+                drag={first ? "x" : false}
+                dragElastic={0.8}
+                dragConstraints={dragBoundaries}
+                onDragEnd={(_, info) => {
+                  const dragMax = info.point.x;
 
-                if (Math.abs(dragMax) > DRAG_TRIGGER) {
-                  // rate the card w/ the lowest grade
-                  onCardReview(card, 0);
-                }
-              }}
-              style={{
-                cursor: first ? "grab" : "pointer",
-              }}
-              animate={{
-                zIndex: cards.length - i,
-              }}
-            >
-              <CardWithFlip
-                card={card}
-                noShadow={!first}
-                onCardReview={onCardReview} // TODO avoid drilling
-              />
-            </motion.div>
-          );
-        })}
-      </motion.div>
+                  if (Math.abs(dragMax) > DRAG_TRIGGER) {
+                    // rate the card w/ the lowest grade
+                    onCardReview(card, 0);
+                  }
+                }}
+                style={{
+                  cursor: first ? "grab" : "pointer",
+                }}
+                animate={{
+                  zIndex: cards.length - i,
+                }}
+              >
+                <CardWithFlip
+                  card={card}
+                  noShadow={!first}
+                  onCardReview={onCardReview} // TODO avoid drilling
+                />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </div>
 
+      {/* SessionInfo will go here */}
       {children}
     </div>
   );
