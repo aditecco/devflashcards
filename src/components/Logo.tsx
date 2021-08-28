85d22a063e1f0ba161593c11f430d7ededde6d49
Alessandro Di Tecco <alessandro@ditecco.me>
2021-08-28 23:30:42 +0200

    Refactored Logo.tsx w/ embedded link to home

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,13 +6,25 @@ import * as React from "react";
 import { PropsWithChildren, ReactElement } from "react";
 import MaterialIcon from "./MaterialIcon";
 import { css } from "@emotion/react";
+import { navigate } from "gatsby";
 
 type OwnProps = {};
 
 export default function Logo({}: PropsWithChildren<OwnProps>): ReactElement | null {
   return (
-    <div
+    <a
+      onClick={() => navigate("/")}
       css={css`
+        text-decoration: none;
+        color: inherit;
+        cursor: pointer;
+        transition: transform 0.15s ease-in-out;
+
+        &:hover {
+          color: inherit;
+          transform: scale(1.05);
+        }
+
         h1 {
           font-size: inherit;
           margin: 0;
@@ -30,6 +42,6 @@ export default function Logo({}: PropsWithChildren<OwnProps>): ReactElement | nu
         <MaterialIcon icon={"graphic_eq"} />
         <span className="navbar-logotype">DevFlashcards</span>
       </h1>
-    </div>
+    </a>
   );
 }
