84de893f0cd8435554c14c7746b8aabee2657e4d
Alessandro Di Tecco <alessandro@ditecco.me>
2021-08-29 16:36:50 +0200

    Fixed theme.ts TS issues

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -4,16 +4,21 @@ Theme types, WIP
 
 import "@emotion/react";
 
-// TODO define types
-type ThemeColorDefinition = {};
+type ThemeColors = {
+  accent?: Record<string, string | Record<string, string>>;
+  utility?: Record<string, string | Record<string, string>>;
+  stroke?: Record<string, string | Record<string, string>>;
+  typography?: Record<string, string | Record<string, string>>;
+  background?: Record<string, string | Record<string, string>>;
+};
 
-type ThemeFontDefinition = {};
+type ThemeFonts = {};
 
 declare module "@emotion/react" {
   export interface Theme {
     space: Record<string, string>;
     fontSizes: Record<string, string>;
-    colors: Record<string, string>;
+    colors: ThemeColors;
     fonts: Record<string, string>;
     fontWeights: Record<string, string>;
     lineHeights: Record<string, string>;
