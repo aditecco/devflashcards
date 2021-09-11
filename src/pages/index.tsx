020cfe1a52d98c5e831e5f1ccb8a6700cb75a418
Alessandro Di Tecco <alessandro@ditecco.me>
2021-09-11 17:25:35 +0200

    Updated Home with page query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,39 @@ Home
 
 import * as React from "react";
 import Layout from "../components/Layout";
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
+import { slugify } from "../utils";
 
-const Home = () => {
+const Home = ({ data }) => {
+  const {
+    allDirectory: { edges: decks },
+  } = data ?? {};
+
   return (
     <Layout>
-      <div>TODO Choose a deck</div>
-      {/* TODO should be: deck/<deck name or ID>/ */}
-      <Link to={"/deck"}>Go to default deck</Link>
+      <ul>
+        {decks?.map?.((deck) => (
+          <li>
+            <Link to={`/decks/` + slugify(deck?.node?.name)}>
+              {deck?.node?.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 };
 
 export default Home;
+
+export const query = graphql`
+  {
+    allDirectory(filter: { name: { nin: ["pages", "decks"] } }) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`;
