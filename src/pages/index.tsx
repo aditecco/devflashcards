9bafb2d093d67660cbe7894ae356f3ca684390c7
Alessandro Di Tecco <alessandro@ditecco.me>
2021-09-18 13:33:04 +0200

    Updated home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,12 @@ Home
 import * as React from "react";
 import Layout from "../components/Layout";
 import { graphql, Link } from "gatsby";
-import { slugify } from "../utils";
+import { slugify, truncate } from "../utils";
 import { Container } from "../components/Container";
 import { Grid } from "../components/Grid";
 import SimpleCard from "../components/SimpleCard";
+import { MutedText } from "../components/MutedText";
+import { Heading } from "../components/Heading";
 
 const Home = ({ data }) => {
   const {
@@ -18,16 +20,26 @@ const Home = ({ data }) => {
   return (
     <Layout>
       <Container>
+        <Heading style={{ textAlign: "center" }}>Pick a deck</Heading>
+
         <Grid as={"ul"} style={{ padding: "1.5rem 0" }}>
-          {decks?.map?.((deck, i) => (
-            <li key={i}>
-              <SimpleCard>
-                <Link to={`/decks/` + slugify(deck?.node?.name)}>
-                  {deck?.node?.name}
-                </Link>
-              </SimpleCard>
-            </li>
-          ))}
+          {decks?.map?.((deck, i) => {
+            const displayIndex = i + 1;
+
+            return (
+              <li key={i}>
+                <SimpleCard>
+                  <Link to={`/decks/` + slugify(deck?.node?.name)}>
+                    <MutedText size={16} style={{ marginRight: 8 }}>
+                      {displayIndex < 10 ? "0" + displayIndex : displayIndex}
+                    </MutedText>
+
+                    {truncate(deck?.node?.name)}
+                  </Link>
+                </SimpleCard>
+              </li>
+            );
+          })}
         </Grid>
       </Container>
     </Layout>
@@ -42,6 +54,7 @@ export const query = graphql`
       edges {
         node {
           name
+          id
         }
       }
     }
