657e3d04cab7dcc857170a779416b44afd1160f5
Alessandro Di Tecco <alessandro@ditecco.me>
2021-09-18 13:32:07 +0200

    Updated Footer.tsx styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,21 @@ import { Container } from "./Container";
 
 const Footer = styled.footer`
   font-size: small;
-  padding: 0.6rem 1rem;
+  padding: 1.25rem 1rem;
+  color: ${(p) => p?.theme?.colors?.typography?.[2]};
+  text-align: center;
+  border-top: 1px solid ${(p) => p?.theme?.colors?.stroke?.[2]};
+
+  a {
+    color: inherit;
+    text-decoration: none;
+    border-bottom: 1px dashed ${(p) => p?.theme?.colors?.typography?.[2]};
+
+    &:hover {
+      color: ${(p) => p?.theme?.colors?.utility?.black};
+      border-bottom: 1px dashed ${(p) => p?.theme?.colors?.utility?.black};
+    }
+  }
 `;
 
 export default function ({ children }) {
