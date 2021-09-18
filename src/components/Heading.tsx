743717fe82c72af05b691592924aef1e82028151
Alessandro Di Tecco <alessandro@ditecco.me>
2021-09-18 13:31:49 +0200

    Created MutedText.tsx & Heading.tsx

---
+++ src/components/Heading.tsx
@@ -0,0 +1,10 @@
+/* ---------------------------------
+Heading
+--------------------------------- */
+
+import styled from "@emotion/styled";
+
+export const Heading = styled.h1`
+  font-size: 2rem;
+  font-weight: 300;
+`;
