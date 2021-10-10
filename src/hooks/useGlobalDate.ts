40f71c9f83a252461924391aeda48a98d8052d88
Alessandro Di Tecco <alessandro@ditecco.me>
2021-10-10 16:07:03 +0200

    feature/refactor_with_context | Refactored CurrentTime.tsx as useGlobalDate.ts

---
+++ src/hooks/useGlobalDate.ts
@@ -0,0 +1,21 @@
+/* ---------------------------------
+useGlobalDate
+--------------------------------- */
+
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+
+// Session start
+const INITIAL_DATE = dayjs();
+
+export default function useGlobalDate() {
+  const [currentDate, setCurrentDate] = useState(INITIAL_DATE);
+
+  useEffect(() => {
+    const timer = setInterval(() => setCurrentDate(dayjs()), 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return { initial: INITIAL_DATE, current: currentDate };
+}
