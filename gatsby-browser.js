72ba6bb05b903c1db0767cce2bd976224cf43b1c
Alessandro Di Tecco <alessandro@ditecco.me>
2021-10-10 16:06:12 +0200

    feature/refactor_with_context | Refactored gatsby-browser.js with App.tsx wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,6 @@ gatsby-browser
 --------------------------------- */
 
 import React from "react";
-import { ThemeProvider, Global, css } from "@emotion/react";
-import theme from "./src/theme";
 
 // fonts
 import "@fontsource/material-icons/index.css";
@@ -15,40 +13,6 @@ import "@fontsource/lato/400-italic.css";
 // 3rd party CSS
 import "normalize.css/normalize.css";
 import "prismjs/themes/prism-tomorrow.css";
-import { baseFontStack } from "./src/constants/css-vars";
-import { AnimatePresence } from "framer-motion";
+import { App } from "./src/App";
 
-export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={theme}>
-    <Global
-      styles={css`
-        *,
-        *::before,
-        *::after {
-          box-sizing: border-box;
-        }
-
-        body {
-          font-family: ${baseFontStack};
-          margin: 0;
-          padding: 0;
-
-          // WIP https://css-tricks.com/the-current-state-of-styling-scrollbars/
-          &::-webkit-scrollbar {
-            width: 10px;
-          }
-
-          &::-webkit-scrollbar-track {
-            box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-          }
-
-          &::-webkit-scrollbar-thumb {
-            background-color: ${theme.colors.accent[1]};
-            outline: 1px solid slategrey;
-          }
-        }
-      `}
-    />
-    <AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
-  </ThemeProvider>
-);
+export const wrapRootElement = ({ element }) => <App>{element}</App>;
---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+/* ---------------------------------
+App
+--------------------------------- */
+
+import * as React from "react";
+import useGlobalDate from "./hooks/useGlobalDate";
+import useGlobalSession from "./hooks/useGlobalSession";
+import { css, Global, ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+import { baseFontStack } from "./constants/css-vars";
+import { DateContext, SessionContext } from "./context";
+import { AnimatePresence } from "framer-motion";
+
+type OwnProps = {};
+
+export const App: React.FC<OwnProps> = ({ children }) => {
+  const currentDate = useGlobalDate();
+  const session = useGlobalSession();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+          }
+
+          body {
+            font-family: ${baseFontStack};
+            margin: 0;
+            padding: 0;
+
+            // WIP https://css-tricks.com/the-current-state-of-styling-scrollbars/
+            &::-webkit-scrollbar {
+              width: 10px;
+            }
+
+            &::-webkit-scrollbar-track {
+              box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+            }
+
+            &::-webkit-scrollbar-thumb {
+              background-color: ${theme.colors.accent[1]};
+              outline: 1px solid slategrey;
+            }
+          }
+        `}
+      />
+
+      <DateContext.Provider value={currentDate}>
+        <SessionContext.Provider value={session}>
+          <AnimatePresence exitBeforeEnter>{children}</AnimatePresence>
+        </SessionContext.Provider>
+      </DateContext.Provider>
+    </ThemeProvider>
+  );
+};
